54d2c5ac52237a5c3847e0b9d968a0f4b4dcaac0
Ali Faheem <alifaheem94@gmail.com>
2023-07-12 17:37:00 +0500

    implemented ram check

---
+++ Database_datainput.py
@@ -0,0 +1,56 @@
+# This is a sample Python script.
+import time
+import psutil
+from datetime import datetime
+import mysql.connector
+
+from util import *
+
+
+# This function inserts ranm usage values to local db
+def insert_values(mydb, current_time, ram_usage):
+
+    # creating query to insert the rmusage values in the db
+    mySql_insert_query = "INSERT INTO ram_values (TIME,RAM) VALUES (%s,%s)"
+    mycursor = mydb.cursor()  # cursor object to communicate with db
+
+
+    # creating record to add to local db
+    record = (current_time, psutil.virtual_memory()[2])
+
+    try:
+        # Inserting values to local db
+        mycursor.execute(mySql_insert_query, record)
+        mydb.commit()
+
+    except mysql.connector.Error as error:
+        print("Data insertion failed", error)
+        mydb.close() #closing the connection
+
+
+def main():
+    try:
+        mydb = connect_to_db()
+    except mysql.connector.Error as error:
+        print("failed to connect", error)
+        return None
+
+
+    while True:
+        # getting current time
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+
+        # getting current ram usage
+        ram_usage = psutil.virtual_memory()[2]
+
+        # inserting values to db
+        insert_values(mydb, current_time, ram_usage)
+
+        time.sleep(15)
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ flask_code.py
@@ -0,0 +1,60 @@
+import mysql.connector
+from util import *
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+# Getting ram usage at particular time from local db
+def get_html_results(time):
+    mydb = connect_to_db()
+    mycursor = mydb.cursor()
+    query = '''SELECT * FROM ram_values
+                           WHERE TIME = (
+                           SELECT TIME
+                           FROM ram_values
+                           ORDER BY ABS(TIMEDIFF(TIME, %s))
+                           LIMIT 1
+                           );'''
+
+    try:
+        mycursor.execute(query, (time,))
+        data = mycursor.fetchall()
+    except mysql.connector.Error as error:
+        print("Data retrieval failed", error)
+        mydb.close()
+        return None
+
+    mydb.close()
+
+    res = '''
+    <table border="1">
+        <tr>
+            <td>Time</td>
+            <td>{}</td>
+        </tr>
+        <tr>
+            <td>Ram Usage</td>
+            <td>{}%</td>
+        </tr>
+    </table>'''.format(str(data[0][1]), str(data[0][2]))
+
+
+
+    return res
+
+
+@app.route('/ram/<time>')
+def get_results(time):
+    try:
+        mydb = connect_to_db()
+    except mysql.connector.Error as error:
+        print("failed to connect", error)
+        return error
+    res = get_html_results(time)
+    return res
+
+
+if __name__ == '__main__':
+   app.run()
