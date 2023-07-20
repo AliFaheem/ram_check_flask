e8eca7e33572a6abb9cb553ffea3ddc4f7ff3bfb
Ali Faheem <alifaheem94@gmail.com>
2023-07-20 16:16:34 +0500

    Modular code with json output

--- util.py
+++ util.py
@@ -2,17 +2,20 @@ from dotenv import dotenv_values
 import mysql.connector
 
 config = dotenv_values("credentials.env")
+# host = 'host.docker.internal',
+
 
 def connect_to_db():
     try:
+        print(config)
         mydb = mysql.connector.connect(
             host=config["DB_HOST"],
             user=config["DB_USER"],
             password=config["DB_PASS"],
-            port = config["DB_PORT"],
-            database = config["DB_NAME"]
+            port=config["DB_PORT"],
+            database=config["DB_NAME"],
         )
     except mysql.connector.Error as error:
-        print("failed to connect",error)
+        print("failed to connect", error)
         return error
     return mydb
